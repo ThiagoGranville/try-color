15af13650a0b77f7ffe9f86a3ae7d1514e6d7e46
Thiago Granville <thiagogranville@gmail.com>
2020-11-14 17:28:57 -0300

    Requisito 5: Adicionado event listener dos circulos

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const bolas = document.querySelectorAll('.ball')
+const bolas = document.querySelectorAll('.ball');
+const textoCor = document.querySelector('#rgb-color');
+const textoResposta = document.querySelector('#answer');
+const cores = [];
 
-function numeroAleatorio() {
-  const numero = Math.floor(Math.random() * 256);
+function numeroAleatorio(num) {
+  const numero = Math.floor(Math.random() * num);
   return numero;
-};
+}
 
-function corAleatoria(){
-  const rgb = `rgb(${numeroAleatorio()}, ${numeroAleatorio()}, ${numeroAleatorio()})`;
+function corAleatoria() {
+  const rgb = `rgb(${numeroAleatorio(256)}, ${numeroAleatorio(
+    256
+  )}, ${numeroAleatorio(256)})`;
   return rgb;
 }
 
-for (let bola = 0; bola < bolas.length; bola++){
-  bolas[bola].style.backgroundColor = corAleatoria();
+function cliqueNaBola(event) {
+  const corSelecionada = event.target.style.backgroundColor;
+  if (corSelecionada === textoCor.textContent) {
+    textoResposta.textContent = 'Acertou!';
+  } else {
+    textoResposta.textContent = 'Errou! Tente novamente!';
+  }
 }
+
+for (let bola = 0; bola < bolas.length; bola += 1) {
+  const cor = corAleatoria();
+  bolas[bola].style.backgroundColor = cor;
+  cores[bola] = cor;
+  bolas[bola].addEventListener('click', cliqueNaBola);
+}
+
+function adicionaCorAleatoria() {
+  textoCor.textContent = cores[numeroAleatorio(6)];
+}
+
+adicionaCorAleatoria();
